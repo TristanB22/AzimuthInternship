608fc953823085cc25954a678fb6c758007ca3d6
Tristan Brigham <tristanbrigham@Tristans-MBP.localdomain>
2021-06-10 12:38:07 -0400

    First Attempt at the First Part of the Code

---
+++ OpenCVProject1/ImageProcessing1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import cv2 as cv
+import matplotlib.pyplot as plt
+import matplotlib.image as mping
+import numpy as np
+img = cv.imread("/Users/tristanbrigham/GithubProjects/AzimuthInternship/OpenCVProject1/White_text_Black_Background.jpg")
+imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+imgray = cv.GaussianBlur(imgray, (5, 5), 0)
+thresh,imgBW = cv.threshold(imgray, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+plt.imshow(imgBW, cmap = "gray")
+cv.waitKey(0)
---
+++ OpenCVProject1/testingOpeningImage.py
@@ -0,0 +1,6 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import matplotlib.image as mping
+import numpy as np
+
+img = cv.imread("/Users/tristanbrigham/GithubProjects/AzimuthInternship/OpenCVProject1/White_text_Black_Background.jpg")
