c3e0179b00c0ae6134c6b16c827d252136d8d1f4
TristanB22 <67669475+TristanB22@users.noreply.github.com>
2021-06-10 12:39:33 -0400

    Delete testingOpeningImage.py

--- OpenCVProject1/testingOpeningImage.py
+++
@@ -1,6 +0,0 @@
-import cv2 as cv
-import matplotlib.pyplot as plt
-import matplotlib.image as mping
-import numpy as np
-
-img = cv.imread("/Users/tristanbrigham/GithubProjects/AzimuthInternship/OpenCVProject1/White_text_Black_Background.jpg")
