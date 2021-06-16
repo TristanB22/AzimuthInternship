e71a2c8184604876b7949aeef165b96c24a6efcf
Tristan Brigham <tristanbrigham@Tristans-MBP.localdomain>
2021-06-16 17:06:41 -0400

    Updating Project

--- OpenCVProject2/CameraOpenCV.py
+++ OpenCVProject2/CameraOpenCV.py
@@ -6,14 +6,19 @@ def detectAndDisplay(frame):
     frame_gray = cv.equalizeHist(frame_gray)
     #-- Detect faces
     faces = face_cascade.detectMultiScale(frame_gray)
+
     for (x,y,w,h) in faces:
-        center = (x + w//2, y + h//2)
-        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
-        faceROI = frame_gray[y:y+h,x:x+w]
+        # center = (x + w//2, y + h//2)
+        # frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
+        frame = cv.putText(frame, "Tristan", (y, y), cv.FONT_HERSHEY_PLAIN, 5, (0, 0, 255), 2)
+        frame = cv.line(frame, (x, y), (y, y), (0, 0, 255), 1)
+        faceROI = frame[y:y+h,x:x+w]
+        faceROI = cv.GaussianBlur(faceROI, (51, 51), 30)
+        frame[y:y+faceROI.shape[0], x:x+faceROI.shape[1]] = faceROI
 
     cv.imshow('Capture - Face detection', frame)
 parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
-parser.add_argument('--face_cascade', help='Path to face cascade.', default='data/haarcascades/haarcascade_frontalface_alt.xml')
+parser.add_argument('--face_cascade', help='Path to face cascade.', default='haarcascade_frontalface_alt.xml')
 parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
 args = parser.parse_args()
 face_cascade_name = args.face_cascade
@@ -34,5 +39,5 @@ while True:
         print('--(!) No captured frame -- Break!')
         break
     detectAndDisplay(frame)
-    if cv.waitKey(10) == 27:
+    if cv.waitKey(10) & 0xFF == ord('q'):
         break
