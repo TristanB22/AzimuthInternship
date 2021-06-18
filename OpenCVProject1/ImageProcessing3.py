cf5e13b648ea945798568588016a5199b7936570
Tristan Brigham <tristanbrigham@Tristans-MBP.localdomain>
2021-06-18 15:23:26 -0400

    Fixing 'i' and 'j' issue

--- OpenCVProject1/ImageProcessing3.py
+++ OpenCVProject1/ImageProcessing3.py
@@ -9,10 +9,23 @@ import numpy as np
 img = cv.imread("/Users/tristanbrigham/GithubProjects/AzimuthInternship/OpenCVProject1/White_text_Black_Background.jpg")
 imgray = img.copy()
 imgray = cv.cvtColor(imgray, cv.COLOR_BGR2GRAY)
-imgray = cv.GaussianBlur(imgray, (5, 5), 0)
-thresh = cv.threshold(imgray, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+imgray = cv.GaussianBlur(imgray, (7, 7), 0)
+
+# imgTemp = imgray[0 : int(29 * imgray.shape[0] / 30)]
+# imgTemp2 = imgray[int(29 * imgray.shape[0] / 30) : imgray.shape[0]]
+# print(dtype(imgray))
+
+# imgShift = np.zeros((imgray.shape[0], imgray.shape[1]), dtype=np.int)
+
+# imgShift[0 : int(imgray.shape[0] / 30)] = imgTemp2
+# imgShift[int(imgray.shape[0] / 30) : imgray.shape[0]] = imgTemp
+
+# imgray = cv.addWeighted(imgray, 1, imgShift, 1, 0)
+
+imgray = cv.dilate(imgray, (3, 50), iterations = 20)
+thresh = cv.threshold(imgray, 0, 127, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
 # imgray = cv.Canny(imgray, 250, 254)
-contours, hierarchy = cv.findContours(imgray, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+contours, _ = cv.findContours(imgray, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 imgray = cv.cvtColor(imgray, cv.COLOR_GRAY2BGR)
 cv.drawContours(imgray, contours, -1, (255, 255, 255), 10)
 contours = contours[0] if len(contours)==2 else contours
@@ -23,10 +36,10 @@ regionOfInterest = 0
 for contour in contours:
     x, y, w, h = cv.boundingRect(contour)
     ROI = imgray[y:y + h, x:x + w]
-    cv.rectangle(imgray, (x, y), (x + w, y + h), (0, 255, 0), 5)
-    cv.imwrite("ROI_0-{}.png".format(regionOfInterest), ROI)
+    cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 5)
+    # cv.imwrite("ROI_0-{}.png".format(regionOfInterest), ROI)
     regionOfInterest += 1
-cv.imshow("Bounded", imgray)
-cv.waitKey(0)
+cv.imshow("Bounded Original", img)
+cv.waitKey(15000)
 cv.destroyAllWindows()
 
