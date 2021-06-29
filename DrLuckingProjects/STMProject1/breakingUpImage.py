7aeca9d3375f37eb0d6b885935e455ff423ebd2f
Tristan Brigham <tristan.brigham@sps.edu>
2021-06-29 19:10:52 +0300

    Updating Code for STM File

--- DrLuckingProjects/STMProject1/breakingUpImage.py
+++ DrLuckingProjects/STMProject1/breakingUpImage.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2 as cv
 import os
+import tensorflow as tf
+from tensorflow import keras
 
 img = plt.imread("/Users/tristanbrigham/GithubProjects/AzimuthInternship/DrLuckingProjects/STMProject1/firstSTM.png")
 print("Size: {}".format(img.shape))
+
+finalArray = np.zeros((336, 17, 15, 4))
 
 yAmt = int(img.shape[1] /  14)
 offsetY = int(yAmt / 2)
@@ -15,17 +20,16 @@ offsetX = xAmt     #uncomment the above line and comment this one to add overlap
 count = 0
 x = xAmt
 
-for i in range(500):
-    os.system("rm Portion{}.png".format(i))
-
 while x < img.shape[1]:
     y = yAmt
     while y < img.shape[0] - offsetY:
         imgTemp = img[y - yAmt:y, x - xAmt:x]
-        plt.imsave("Portion{}.png".format(count), imgTemp)
+        finalArray[count] = imgTemp
         count += 1
         y += offsetY
         if(count > 1000):
             exit()
     x += offsetX
+    
+print(finalArray.shape)
 print("----------------------------—SUCCESS----------------------------—")
