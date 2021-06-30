9c68d3e658bcb32d7dde4e8cd46cd51773e33bbc
Tristan Brigham <tristan.brigham@sps.edu>
2021-06-30 20:09:48 +0300

    Updating license plate project

--- MrCrutcherProjects/LicensePlateProject/main.py
+++ MrCrutcherProjects/LicensePlateProject/main.py
@@ -1,38 +1,42 @@
 import cv2 as cv
 import numpy as np
-import pytesseract
+import matplotlib.pyplot as plt
+# import pytesseract
 
 img = cv.imread("/Users/tristanbrigham/GithubProjects/AzimuthInternship/MrCrutcherProjects/LicensePlateProject/licensePlate.jpeg")
+img = cv.resize(img, (640, 480))
 
 imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+imgray = cv.dilate(imgray, (3, 3), iterations=2)
 imgray = cv.GaussianBlur(imgray, (5, 5), 0)
-
-# imgray = cv.dilate(imgray (3, 50), iterations = 5)
-# imgray = cv.Canny(imgray, 50)
-ret, thresh = cv.threshold(imgray, 100, 255, cv.THRESH_OTSU)
+hist = cv.calcHist(imgray, [0], None, [256], [0, 256])
+ret, thresh = cv.threshold(imgray, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+# imgray = cv.Canny(imgray, 100, 255)
 # thresh = cv.bitwise_not(thresh)
-cv.imshow("thresh", thresh)
+# cv.imshow("thresh", thresh)
 
 contours, _ = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-# imgray = cv.drawContours(imgray, contours, -1, (0, 255, 0), thickness=5)
+img = cv.drawContours(img, contours, -1, (0, 255, 0), thickness=5)
 
-c = max(contours, key = cv.contourArea)
-x, y, w, h = cv.boundingRect(c)
-largestContour = img[y : y + h, x : x + w]
-cv.imshow("LargestContour", largestContour)
+# c = max(contours, key = cv.contourArea)
+# x, y, w, h = cv.boundingRect(c)
+# largestContour = imgray[y : y + h, x : x + w]
+# largestContour = cv.bitwise_not(largestContour)
+# cv.imshow("LargestContour", largestContour)
 
-
-thresh = cv.cvtColor(thresh, cv.COLOR_GRAY2RGB)
+imgray = cv.cvtColor(thresh, cv.COLOR_GRAY2RGB)
 print(len(contours))
 
 for contour in contours:
     x, y, w, h = cv.boundingRect(contour)
-    cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness = 5)
-    cv.rectangle(imgray, (x, y), (x + w, y + h), (0, 255, 0), thickness = 5)
-    cv.rectangle(thresh, (x, y), (x + w, y + h), (0, 255, 0), thickness = 5)
+    cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness = 3)
+    cv.rectangle(imgray, (x, y), (x + w, y + h), (0, 255, 0), thickness = 3)
+    # cv.rectangle(thresh, (x, y), (x + w, y + h), (0, 255, 0), thickness = 3)
 
 cv.imshow("Img with Contours", img)
 cv.imshow("Grayscale", imgray)
-cv.imshow("Thresh", thresh)
+# cv.imshow("Thresh", thresh)
+plt.plot(hist)
+plt.show()
 if cv.waitKey(0) & 0xFF == ord('q'):
     exit()
