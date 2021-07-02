f2480b1a35470712660e780e9e0b8d86f2568226
Tristan Brigham <tristan.brigham@sps.edu>
2021-07-02 23:15:30 +0300

    Updating License Plate project

--- MrCrutcherProjects/LicensePlateProject/main.py
+++ MrCrutcherProjects/LicensePlateProject/main.py
@@ -1,14 +1,16 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+import imutils
 # import pytesseract
 
 class FindPlate:
     def __init__(self):
         self.min = 1000
         self.max = 60000
-
-        self.img = cv.resize(cv.imread("/Users/tristanbrigham/GithubProjects/AzimuthInternship/MrCrutcherProjects/LicensePlateProject/licensePlate3.jpeg"), (640, 480))
+        self.maxAspect = 1
+        self.minAspect = 0.15
+        self.img = cv.resize(cv.imread("/Users/tristanbrigham/GithubProjects/AzimuthInternship/MrCrutcherProjects/LicensePlateProject/licensePlate4.jpeg"), (640, 480))
         self.element_structure = cv.getStructuringElement(shape=cv.MORPH_RECT, ksize=(5, 5))
 
     def preprocessing(self):
@@ -26,8 +28,8 @@ class FindPlate:
         element = self.element_structure
         morph_thresh = thresh.copy()
         cv.morphologyEx(morph_thresh, op=cv.MORPH_CLOSE, kernel=element, dst=morph_thresh, iterations=1)
-        cv.morphologyEx(morph_thresh, op=cv.MORPH_ERODE, kernel=element, dst=morph_thresh, iterations=2)
-        cv.morphologyEx(morph_thresh, op=cv.MORPH_OPEN, kernel=element, dst=morph_thresh, iterations=2)
+        # cv.morphologyEx(morph_thresh, op=cv.MORPH_ERODE, kernel=element, dst=morph_thresh, iterations=2)
+        # cv.morphologyEx(morph_thresh, op=cv.MORPH_OPEN, kernel=element, dst=morph_thresh, iterations=2)
 
         return morph_thresh
 
@@ -41,40 +43,53 @@ class FindPlate:
         edged = cv.Canny(gray, 10, 50)
         cv.imshow("Gray", gray)
         cv.imshow("Canny", edged)
-
         return edged
 
-    def extract_large_contours(self, after_preprocess, keep=10):
-        contours, _ = cv.findContours(after_preprocess, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    def extract_largest_contours(self, after_preprocess, keep=10):
+        contours= cv.findContours(after_preprocess.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         print("Length of Contours: {}".format(len(contours)))
-        contours = sorted(contours, key=cv.contourArea, reverse=True)
-        return contours[:40]
+        return contours[:keep]
 
-    def extract_contours(self, after_preprocess, keep=10):
-        contours, _ = cv.findContours(after_preprocess, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    def findRectsandContours(self, after_preprocess, keep=-1):
+        contours= cv.findContours(after_preprocess.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         print("Length of Contours: {}".format(len(contours)))
-        return contours
+        contours = imutils.grab_contours(contours)
+        contours = sorted(contours, key=cv.contourArea, reverse=True)
+        if keep == -1:
+            keep=len(contours)
+        for c in contours[:keep]:
+            peri = cv.arcLength(c, True)    
+            approx = cv.approxPolyDP(c, 0.02 * peri, True,)
+            x, y, w, h = cv.boundingRect(c)
+            ratio = h / w
+            if ratio < self.maxAspect and ratio > self.minAspect and len(approx) == 4:
+                cv.drawContours(self.img, [c], -1, (0, 255, 0))
+                cv.rectangle(self.img, (x, y), (x + w, y + h), (0, 255, 0))
+                return c
 
 # class TensorFlow:
 
 
 image = FindPlate()
-preprocessing_image = image.preprocessing()
+preprocessing_image = image.preprocessingCanny()
 
-# contours = image.extract_large_contours(preprocessing_image)
-contours = image.extract_contours(preprocessing_image)
-contourImg = image.img.copy()
+rectangle = image.findRectsandContours(preprocessing_image, keep=20)
+# contours = image.extract_contours(preprocessing_image)
 
 preprocessing_image = cv.cvtColor(preprocessing_image, cv.COLOR_GRAY2BGR)
 
-for contour in contours:
-    x, y, w, h = cv.boundingRect(contour)
-    cv.rectangle(preprocessing_image, (x, y), (x + w, y + h), (0, 255, 0), thickness = 3)
-    cv.rectangle(image.img, (x, y), (x + w, y + h), (0, 255, 0), thickness = 3)
+x, y, w, h = cv.boundingRect(rectangle)
+cv.rectangle(preprocessing_image, (x, y), (x + w, y + h), (0, 255, 0), thickness = 20)
+cv.rectangle(image.img, (x, y), (x + w, y + h), (0, 255, 0), thickness = 20)
+
+# for contour in contours:
+#     x, y, w, h = cv.boundingRect(contour)
+#     cv.rectangle(preprocessing_image, (x, y), (x + w, y + h), (0, 255, 0), thickness = 1)
+#     cv.rectangle(image.img, (x, y), (x + w, y + h), (0, 255, 0), thickness = 1)
 
 
 cv.imshow("Img", image.img)
 cv.imshow("Preprocess", preprocessing_image)
 # cv.imshow("contours", cv.drawContours(contourImg, contours, -1, (0, 255, 0)))
-if cv.waitKey(0) & 0xFF == ord('q'):
+if cv.waitKey(15000) & 0xFF == ord('q'):
     exit()
