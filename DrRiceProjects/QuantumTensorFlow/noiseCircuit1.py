8da2a22925e20c784b4be88eda24c0c8740c39b6
Tristan Brigham <tristan.brigham@sps.edu>
2021-07-19 09:11:02 +0300

    Updating Quantum Circuit Project

---
+++ DrRiceProjects/QuantumTensorFlow/noiseCircuit1.py
@@ -0,0 +1,55 @@
+import cirq
+import numpy as np
+
+class BitAndPhaseFlipChannel(cirq.SingleQubitGate):
+    def __init__(self, p: float) -> None:
+        self._p = p
+
+    def _mixture_(self):
+        ps = [1.0 - self._p, self._p]
+        ops = [cirq.unitary(cirq.I), cirq.unitary(cirq.Y)]
+        return tuple(zip(ps, ops))
+
+    def _has_mixture_(self) -> bool:
+        return True
+
+    def _circuit_diagram_info_(self, args) -> str:
+        return f"BitAndPhaseFlip({self._p})"
+
+bit_phase_flip = BitAndPhaseFlipChannel(p=0.05)
+
+def noisyCircuit(measure=True, depolarize=True, bitPhase=True, bitEnd = True):
+    qreg = cirq.LineQubit.range(4)
+    depInd = 1
+    bitPhaseFlipInd = 2
+    measureInd = 4
+
+    circ = cirq.Circuit(
+        cirq.H.on_each(qreg),
+        cirq.qft(*qreg),
+        cirq.qft(*qreg, inverse=True),
+        cirq.reset(qreg[1]),
+    )
+
+    if depolarize:
+        measureInd += 1
+        bitPhaseFlipInd +=  1
+        circ.insert(depInd,  cirq.depolarize(p=0.01).on_each(qreg))
+
+    if bitPhase:
+        measureInd += 1
+        circ.insert(bitPhaseFlipInd,  bit_phase_flip.on_each(qreg[1::2]))
+
+    if measure:
+        circ.insert(measureInd, cirq.measure(*qreg))
+
+    if bitEnd:
+       circ.append(cirq.bit_flip(p=0.07).controlled(1).on(*qreg[2:]))
+
+
+
+    print("Circuit with multiple channels:\n")
+    print(circ)
+
+
+noisyCircuit()
