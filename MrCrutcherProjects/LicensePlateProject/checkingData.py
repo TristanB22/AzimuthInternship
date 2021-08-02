182b8c9f37c128b5d7233ba939ac95738b952172
Tristan Brigham <tristan.brigham@sps.edu>
2021-08-02 11:20:22 +0300

    Updating the checkingData function

--- MrCrutcherProjects/LicensePlateProject/checkingData.py
+++ MrCrutcherProjects/LicensePlateProject/checkingData.py
@@ -1,10 +1,12 @@
+from re import T
 import cv2 as cv
 import numpy as np
 
 file = open("/Users/tristanbrigham/GithubProjects/AI_Training_Data/LicensePlateProject/training_data", "r")
 # cv.imwrite("/Users/tristanbrigham/GithubProjects/AI_Training_Data/LicensePlateProject/0.txt", np.zeros((60, 80)))
 linesarr = file.readlines()
-for count in range(1, 1280):
+print("LENGTH: {}".format(len(linesarr)))
+for count in range(1730, len(linesarr)):
     array = np.loadtxt("/Users/tristanbrigham/GithubProjects/AI_Training_Data/LicensePlateProject/" + str(count) + ".txt")
     if len(array) < 4800:
         print("APPENDING {}".format(4800 - len(array)))
@@ -12,4 +14,12 @@ for count in range(1, 1280):
         array = np.append(array, array2)
         file = "/Users/tristanbrigham/GithubProjects/AI_Training_Data/LicensePlateProject/" + str(count) + ".txt"
         np.savetxt(file, array)
+    array = np.reshape(array, (80, 60))
+    cv.imshow("Image", array)
     print(linesarr[count])
+    cv.waitKey(0)
+
+0
+P
+Z
+
